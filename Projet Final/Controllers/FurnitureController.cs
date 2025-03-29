663200cee5df12a5018f818be1c504af050d775f
lucas-ladeira <lucas.bellinazzi@gmail.com>
2025-03-29 17:51:12 -0400

    Adding the controllers for the models

--- Projet Final/Controllers/FurnitureController.cs
+++ Projet Final/Controllers/FurnitureController.cs
@@ -1,12 +1,95 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Projet_Final.Data.Services;
+using Projet_Final.Models;
 
 namespace Projet_Final.Controllers
 {
 	public class FurnitureController : Controller
 	{
-		public IActionResult Index()
+		private readonly IFurnitureService _service;
+
+		public FurnitureController(IFurnitureService service)
+		{
+			_service = service;
+		}
+
+		// GET: Furniture/
+		public async Task<IActionResult> Index()
+		{
+			// Récupérer la liste des meubles
+			IEnumerable<Furniture> furnitures = await _service.GetAllFurnituresAsync();
+			return View(furnitures);
+		}
+
+		// GET: Furniture/Details/1
+		public async Task<IActionResult> Details(int id)
+		{
+			var furniture = await _service.GetFurnitureByIdAsync(id);
+			if (furniture == null) return View("NotFound");
+			return View(furniture);
+		}
+
+		// GET: Furniture/Create
+		public IActionResult Create()
 		{
 			return View();
+		}
+
+		// POST: Furniture/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name,Description,UrlPicture,Price,Quantity,Comments,TypeFurnitureId")] Furniture furniture)
+		{
+			if (ModelState.IsValid)
+			{
+				if (!await _service.AddFurnitureAsync(furniture))
+				{
+					ModelState.AddModelError("Name", "Le meuble existe déjà");
+					return View(furniture);
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(furniture);
+		}
+
+		// GET: Furniture/Edit/1
+		public async Task<IActionResult> Edit(int id)
+		{
+			var furniture = await _service.GetFurnitureByIdAsync(id);
+			if (furniture == null) return View("NotFound");
+			return View(furniture);
+		}
+
+		// POST: Furniture/Edit/1
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name,Description,UrlPicture,Price,Quantity,Comments,TypeFurnitureId")] Furniture furniture)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(furniture);
+			}
+			await _service.UpdateFurnitureAsync(id, furniture);
+			return RedirectToAction(nameof(Index));
+		}
+
+		// GET: Furniture/Delete/1
+		public async Task<IActionResult> Delete(int id)
+		{
+			var furniture = await _service.GetFurnitureByIdAsync(id);
+			if (furniture == null) return View("NotFound");
+			return View(furniture);
+		}
+
+		// POST: Furniture/Delete/1
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var furniture = await _service.GetFurnitureByIdAsync(id);
+			if (furniture == null) return View("NotFound");
+			await _service.DeleteFurnitureAsync(id);
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
--- Projet Final/Controllers/FurnitureTypeController.cs
+++ Projet Final/Controllers/FurnitureTypeController.cs
@@ -1,14 +1,94 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Projet_Final.Data.Services;
+using Projet_Final.Models;
 
 namespace Projet_Final.Controllers
 {
 	public class FurnitureTypeController : Controller
 	{
 		private readonly IFurnitureTypeService _service;
-		public IActionResult Index()
+
+		public FurnitureTypeController(IFurnitureTypeService service)
+		{
+			_service = service;
+		}
+
+		// GET: FurnitureType/
+		public async Task<IActionResult> Index()
+		{
+			// Récupérer la liste des types de meubles
+			IEnumerable<FurnitureType> furnitureTypes = await _service.GetAllFurnitureTypeAsync();
+			return View(furnitureTypes);
+		}
+
+		// GET: FurnitureType/Details/1
+		public async Task<IActionResult> Details(int id)
+		{
+			FurnitureType furnitureType = await _service.GetFurnitureTypeByIdAsync(id);
+			return View(furnitureType);
+		}
+
+		// GET: FurnitureType/Create
+		public IActionResult Create()
 		{
 			return View();
+		}
+
+		// POST: FurnitureType/Create
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Create([Bind("Id,Name")] FurnitureType furnitureType)
+		{
+			if (ModelState.IsValid)
+			{
+				if (!await _service.AddFurnitureTypeAsync(furnitureType))
+				{
+					ModelState.AddModelError("Name", "Le type de meuble existe déjà");
+					return View(furnitureType);
+				}
+				return RedirectToAction(nameof(Index));
+			}
+			return View(furnitureType);
+		}
+
+		// GET: FurnitureType/Edit/1
+		public async Task<IActionResult> Edit(int id)
+		{
+			var furnitureType = await _service.GetFurnitureTypeByIdAsync(id);
+			if (furnitureType == null) return View("NotFound");
+			return View(furnitureType);
+		}
+
+		// POST: FurnitureType/Edit/1
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> Edit(int id, [Bind("Id,Name")] FurnitureType furnitureType)
+		{
+			if (!ModelState.IsValid)
+			{
+				return View(furnitureType);
+			}
+			await _service.UpdateFurnitureTypeAsync(id, furnitureType);
+			return RedirectToAction(nameof(Index));
+		}
+
+		// GET: FurnitureType/Delete/1
+		public async Task<IActionResult> Delete(int id)
+		{
+			var furnitureType = await _service.GetFurnitureTypeByIdAsync(id);
+			if (furnitureType == null) return View("NotFound");
+			return View(furnitureType);
+		}
+
+		// POST: FurnitureType/Delete/1
+		[HttpPost]
+		[ValidateAntiForgeryToken]
+		public async Task<IActionResult> DeleteConfirmed(int id)
+		{
+			var furnitureType = await _service.GetFurnitureTypeByIdAsync(id);
+			if (furnitureType == null) return View("NotFound");
+			await _service.DeleteFurnitureTypeAsync(id);
+			return RedirectToAction(nameof(Index));
 		}
 	}
 }
